b4ef127505cba9b58491a5b6bf8ef9a639892143
JLP-BioInf <68468769+JLP-BioInf@users.noreply.github.com>
2020-10-21 21:36:26 +0100

    fix RNA structure plotting

--- ui.R
+++ ui.R
@@ -12,19 +12,17 @@ load("./data/dataForShiny2.RData")
 load("./data/clusterPositionsWithStructures.RData")
 myCol = c("#000000","#000000","#000000","#000000",colorRampPalette(brewer.pal(8,"YlOrRd"))(40))
 
-#cols = log2(max(combinedMatListScaleNoReps[["sars"]][["control"]]+1)) / 25
-#myCol2 = myCol[1:round(length(myCol) * cols, digits = 2)]
 
 
 linebreaks <- function(n){HTML(strrep(br(), n))}
 
 ui <- navbarPage(theme = shinytheme("superhero"),
-  title = "SARS2-MERS-COMP",
-                 tabPanel("SARS2-MERS-COMP",
+  title = "SARS-CoV2 COMRADES",
+                 tabPanel("SARS2 MERS Compairson",
                           h1("Contact maps of SARS-CoV2                                  Contact maps of MERS"),
     
                           fluidRow(
-                            splitLayout(cellWidths = "550px", #plotlyOutput("heatSample",width = "500px", height = "400px"), 
+                            splitLayout(cellWidths = "550px", 
                             plotlyOutput("heatSample",width = "500px", height = "450px"),#)),
                             #h1("Contact maps of MERS"),
                             plotlyOutput("heatControl",width = "500px", height = "450px") )),
@@ -41,14 +39,11 @@ ui <- navbarPage(theme = shinytheme("superhero"),
                           linebreaks(5),
                           hr(),
                           hr(),
-                          imageOutput("structure", width = "600px", height = "400px")
+                          plotOutput("structure2", width = "600px", height = "400px")
                           
                           
                  ),#end of tab1
-                 
-                 tabPanel("MRNA",
-                 ),
-                 tabPanel("Download Raw Data",
+                 tabPanel("Data Download",
                  )
                  
                  
