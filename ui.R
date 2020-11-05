b255a5e0e9e46b6a5b238a79591a121d00adc42e
JLP-BioInf <68468769+JLP-BioInf@users.noreply.github.com>
2020-11-05 20:38:00 +0000

    Add more explanation of the heatmaps
    and links to the paper

--- ui.R
+++ ui.R
@@ -19,7 +19,12 @@ linebreaks <- function(n){HTML(strrep(br(), n))}
 ui <- navbarPage(theme = shinytheme("superhero"),
   title = "SARS-CoV2 COMRADES",
                  tabPanel("SARS2 MERS Compairson",
-                          h1("Contact maps of SARS-CoV2                                  Contact maps of MERS"),
+                          h4("This Shiny app allows you to explore the results from the publication: The short- and long-range RNA-RNA Interactome of SARS-CoV-2"),
+                          h4("https://www.cell.com/molecular-cell/fulltext/S1097-2765(20)30782-6"),
+                          h4("https://doi.org/10.1016/j.molcel.2020.11.004"),
+                          h1("Contact maps of SARS-CoV2 and MERS"),
+                          h4("The heatmaps show the clusters of duplex reads for SARS-CoV-2 and MERS. They are interactive, by zooming in one one of the heatmaps the other will zoom to the same co-ordinates. Double click to zoom out again.  "),
+                          
     
                           fluidRow(
                             splitLayout(cellWidths = "550px", 
@@ -29,7 +34,7 @@ ui <- navbarPage(theme = shinytheme("superhero"),
                           linebreaks(5),
                           hr(),
                           hr(),
-                          h1("Clusters Shown in Contact Heatmaps Above:"),
+                          h1("SARS-Cov-2 Clusters Shown in Contact Heatmap Above:"),
                           dataTableOutput("clusterTable"),
                           linebreaks(5),
                           hr(),
@@ -44,7 +49,9 @@ ui <- navbarPage(theme = shinytheme("superhero"),
                           
                  ),#end of tab1
                  tabPanel("Data Download",
-                 )
+                         # h1("Download all Chimeras"),
+                          #dataTableOutput("rawTable"),   
+                          )
                  
                  
                  
